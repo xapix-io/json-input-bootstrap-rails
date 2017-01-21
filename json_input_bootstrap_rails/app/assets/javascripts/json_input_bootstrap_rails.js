f2833c73611427da98681d802374ed947438dc33
oliver <info@pickled-olives.de>
2017-01-20 19:49:51 -0500

    add finetuning

--- json_input_bootstrap_rails/app/assets/javascripts/json_input_bootstrap_rails.js
+++ json_input_bootstrap_rails/app/assets/javascripts/json_input_bootstrap_rails.js
@@ -1,55 +1,54 @@
 $(document).ready(function() {
 
-  $("body").append(`<div id="json-input-modal" class="modal-dialog modal-lg">
-    <div id="json-input-modal-content" class="modal-content">
-      <form id="json-form">
-        <div id="json-rows">
-          <div class="row json-header-row">
-            <div class="key-label col-xs-5">
-              <span>KEY</span>
-            </div>
-            <div class="colon-label col-xs-1">
-              <span>:</span>
-            </div>
-            <div class="value-label col-xs-5">
-              <span>VALUE</span>
+  $("body").append(`<div id="json-input-modal" class="modal fade" aria-labelledby="JsonInputModal" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div id="json-input-modal-content" class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 class="modal-title">JSON Input</h4>
+        </div>
+        <form id="json-form">
+          <div id="json-rows" class="modal-body">
+            <div class="row json-header-row">
+              <label class="control-label col-xs-5">key</label>
+              <label class="control-label col-xs-1">:</label>
+              <label class="control-label col-xs-5">value</label>
             </div>
           </div>
-        </div>
-        <div class="modal-footer">
-          <button id="save-button" type="submit" class="btn btn-success">Save</button>
-          <button id="cancel-button" type="button" class="btn btn-success">Cancel</button>
-          <button id="add-row" type="button" class="btn btn-success">Add a Row</button>
-        </div>
-      </form>
+          <div class="modal-footer">
+            <div class="row">
+              <div class="col-xs-11"></div>
+              <div class="btn-area col-xs-1"><button id="add-row" type="button" class="btn btn-success">+</button></div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>`)
 
   var text_area = $(".json-input-bootstrap-rails")
   var modal = $("#json-input-modal")
-  var cancel_button = $("#cancel-button")
   var save_button = $("#save-button")
   var add_row_button = $("#add-row")
   var json_form = $("#json-form")
   var json_rows = $("#json-rows")
 
   var new_row = `<div class="row json-row">
-    <div class="key-field col-xs-5">
-      <input class="form-control">
-    </div>
-    <div class="colon-holder col-xs-1">
-      <span>:</span>
-    </div>
-    <div class="value-field col-xs-5">
-      <input class="form-control">
-    </div>
-    <div class="delete-area col-xs-1">
-      <button class="delete-button btn" type="button">X</button>
+    <div class="form-group">
+      <div class="key-field col-xs-5"><input class="form-control"></div>
+      <div class="colon-holder col-xs-1"><span>:</span></div>
+      <div class="value-field col-xs-5"><input class="form-control"></div>
+      <div class="delete-area btn-area col-xs-1">
+        <button class="delete-button btn btn-danger" type="button">X</button>
+      </div>
     </div>
   </div>`
 
-
   text_area.click(function() {
+    modal.modal('show');
+    
     json_form.attr("data", this.id)
     var current_text = ""
 
@@ -61,26 +60,23 @@ $(document).ready(function() {
       $(".json-row").remove()
       $.each(current_text, function(key, val){
         json_rows.append(`<div class="row json-row">
-          <div class="key-field col-xs-5">
-            <input class="key-data form-control" value="` + key + `">
-          </div>
-          <div class="colon-holder col-xs-1">
-            <span>:</span>
-          </div>
-          <div class="value-field col-xs-5">
-            <input class="value-data form-control" value="` + val +`">
-          </div>
-          <div class="delete-area col-xs-1">
-            <button class="delete-button btn" type="button">X</button>
+          <div class="form-group">
+            <div class="key-field col-xs-5">
+              <input class="key-data form-control" value="` + key + `">
+            </div>
+            <div class="colon-holder col-xs-1"><span>:</span></div>
+            <div class="value-field col-xs-5">
+              <input class="value-data form-control" value="` + val +`">
+            </div>
+            <div class="delete-area btn-area col-xs-1">
+              <button class="delete-button btn btn-danger" type="button">X</button>
+            </div>
           </div>
         </div>`)
       })
+      json_rows.append(new_row)
     }
-    modal.css("display", "block")
-  })
-
-  cancel_button.click(function() {
-    modal.css("display", "none")
+    modal.modal('show');
   })
 
   add_row_button.click(function(){
@@ -91,18 +87,16 @@ $(document).ready(function() {
     this.parentElement.parentElement.remove()
   });
 
-  json_form.submit(function(event) {
-    event.preventDefault()
+  modal.on('hidden.bs.modal', function () {
     var values = []
     var pairs = {}
-    $.each(event.target.elements, function(index){values.push(event.target.elements[index].value)})
+    $("#json-form input").each(function(index, data){ values.push($(this).val()) })
     values = values.filter(function(n){ return n != "" })
-    for (var i=0; i<values.length; i = i+2) {
+    for (var i=0; i < values.length; i = i+2) {
       pairs[values[i]] = values[i+1]
     }
-    var final = JSON.stringify(pairs)
-    $('[id='+ this.attributes.data.value +']').val(final)
-    modal.css("display", "none")
+    var json = JSON.stringify(pairs)
+    $('[id='+ json_form.attr("data") +']').val(json)
+    modal.modal('hide');
   })
-
 });
